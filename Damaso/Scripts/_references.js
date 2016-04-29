4360fe6372c1e5ea3124f68a72425b25609a45df
Oscar Martinez <omartinezz1988@gmail.com>
2016-04-29 11:13:50 -0400

    burger done

---
+++ Damaso/Scripts/_references.js
@@ -0,0 +1,4 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="default.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="jquery-2.2.1.js" />
---
+++ Damaso/Scripts/default.js
@@ -0,0 +1,41 @@
+﻿$(function () { smoothScroll(); asideMethod(); });
+
+function smoothScroll() {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+};
+
+function asideMethod() {
+    $('#goRight').click(function () {
+        $('#wrapBelt').css('left', '-100%');
+        $('#SecondoryImages').show();
+    });
+
+    $('#goLeft').click(function () {
+        $('#wrapBelt').css('left', '0%');
+        $('#SecondoryImages').hide(600);
+
+    });
+}
+
+$(window).scroll(function () {
+    scrollItDown();
+})
+
+function scrollItDown() {
+    var scooll = $(window).scrollTop();
+    var resul = scooll / 3;
+    $('#HeroSection').css('background-position', '25% ' + resul + 'px');
+    console.log(scooll);
+    console.log(resul);
+}
